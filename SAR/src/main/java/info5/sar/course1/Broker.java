fb8801e85eb4efe1de9c05bdb09643752425205f
noahkohrs <noah@kohrs.fr>
2024-09-12 10:38:48 +0200

    refactor: name stored by default, not part of the abstraction

--- SAR/src/main/java/info5/sar/course1/Broker.java
+++ SAR/src/main/java/info5/sar/course1/Broker.java
@@ -1,31 +1,35 @@
 package info5.sar.course1;
 
 abstract class Broker {
+    /**
+     * The name of the broker.
+     */
+    public final String name;
 
     /**
      * Create a new broker with the given name.
      * A broker is a network abstraction that can accept incoming connections on a given port and connect to hosts.
      *
-     * @param name the name of the broker
+     * @param name the name of the broker.
      */
     Broker(String name) {
-        throw new UnsupportedOperationException("Should be overridden");
+        this.name = name;
     }
 
     /**
      * Accept an incoming connection on the given port.
      *
-     * @param port the port to listen on
-     * @return a channel representing the connection
+     * @param port the port to listen on.
+     * @return a channel representing the connection.
      */
     abstract Channel accept(int port);
 
     /**
      * Connect to a remote host on the given port.
      *
-     * @param host the host to connect to
-     * @param port the port to connect to
-     * @return a channel representing the connection
+     * @param host the host to connect to.
+     * @param port the port to connect to.
+     * @return a channel representing the connection.
      */
     abstract Channel connect(String host, int port);
 }
