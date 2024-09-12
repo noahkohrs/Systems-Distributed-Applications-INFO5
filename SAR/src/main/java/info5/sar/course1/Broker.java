4a6e0805342f33830cbaa625e04c87a5212efcbd
noahkohrs <noah@kohrs.fr>
2024-09-12 10:48:24 +0200

    wip

--- SAR/src/main/java/info5/sar/course1/Broker.java
+++ SAR/src/main/java/info5/sar/course1/Broker.java
@@ -1,6 +1,6 @@
 package info5.sar.course1;
 
-abstract class Broker {
+public abstract class Broker {
     /**
      * The name of the broker.
      */
@@ -32,43 +32,5 @@ abstract class Broker {
      * @return a channel representing the connection.
      */
     abstract Channel connect(String host, int port);
-}
-
-abstract class Channel {
-
-    /**
-     * Read up to length bytes from the channel into the given buffer.
-     * This is a blocking operation, it will return only once at least one byte has been read.
-     *
-     * @param bytes the buffer to read into.
-     * @param offset the offset in the buffer to start reading into.
-     * @param length the maximum number of bytes to read.
-     * @return the number of bytes read, or -1 if the channel is disconnected.
-     */
-    abstract int read(byte[] bytes, int offset, int length);
-
-    /**
-     * Write up to length bytes from the given buffer to the channel.
-     *
-     * This is a blocking operation, it will return only once all the bytes have been written.
-     *
-     * @param bytes the buffer to write from.
-     * @param offset the offset in the buffer to start writing from.
-     * @param length the number of bytes to write.
-     * @return the number of bytes written once it succeed and -1 if the channel is disconnected.
-     */
-    abstract int write(byte[] bytes, int offset, int length);
-
-    /**
-     * Disconnect the channel.
-     * Has no effect if the channel is already disconnected.
-     */
-    abstract void disconnect();
-
-    /**
-     * Check if the channel is disconnected.
-     * @return true if the channel is disconnected, false otherwise.
-     */
-    abstract boolean disconnected();
 }
 
