ccbc4db757a801f65c1088bd2eb27813b4ba6890
noahkohrs <noah@kohrs.fr>
2024-09-12 10:32:55 +0200

    wip

---
+++ SAR/src/test/java/info5/sar/course1/BrokerTest.java
@@ -0,0 +1,18 @@
+package info5.sar.course1;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrokerTest {
+
+    @Nested
+    class BasicScenarios {
+
+        @Test
+        void testBrokerCreation() {
+
+        }
+    }
+}
