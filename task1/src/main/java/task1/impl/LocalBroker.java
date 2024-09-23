3d2cd5648c06026c8fddd4698a7de50a89a49384
noahkohrs <noah@kohrs.fr>
2024-09-23 13:29:32 +0200

    fix: fixing two of Broker connection system issues

--- task1/src/main/java/task1/impl/LocalBroker.java
+++ task1/src/main/java/task1/impl/LocalBroker.java
@@ -24,36 +24,48 @@ public class LocalBroker extends Broker {
     @Override
     public synchronized Channel accept(int port) throws ConnectionFailedException {
         if (rdvs.containsKey(port) && rdvs.get(port).isWaitingForAccept()) {
-            throw new ConnectionFailedException(name);
+            throw new ConnectionFailedException(ConnectionFailedException.Issue.PORT_IN_USE_FOR_THIS_BROKER, String.valueOf(port));
         }
         rdvs.putIfAbsent(port, new RendezVous());
         RendezVous rendezVous = rdvs.get(port);
         Channel c = rendezVous.accept(this);
         rdvs.remove(port);
-        notify();
+        // Notify all brokers that a connection has been established to wake up any threads waiting for a connection
+        BrokerManager.brokers.forEach(b -> {
+                    synchronized (b) {
+                        b.notifyAll();
+                    }
+                });
         return c;
     }
 
     @Override
     public synchronized Channel connect(String host, int port) throws ConnectionFailedException {
-        LocalBroker oppositeBroker = BrokerManager.getBroker(host);
-        Map<Integer, RendezVous> oppositeBrokerRdvs = oppositeBroker.rdvs;
+        LocalBroker hostBroker;
         try {
-            // If there's already a rendezvous on the same port, wait for it to complete
-            RendezVous rendezVous = oppositeBrokerRdvs.get(port);
-            while (rendezVous != null && rendezVous.isWaitingForConnect()) {
-                // Wait for the current connection attempt to complete
-                try {
-                    oppositeBroker.wait();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            oppositeBrokerRdvs.putIfAbsent(port, new RendezVous());
-
-            return rendezVous.connect(this);
+            hostBroker = BrokerManager.getBroker(host);
         } catch (IllegalArgumentException e) {
-            throw new ConnectionFailedException(host);
+            throw new ConnectionFailedException(ConnectionFailedException.Issue.NO_BROKER_WITH_NAME, host);
         }
+        Map<Integer, RendezVous> oppositeBrokerRdvs = hostBroker.rdvs;
+        // If there's already a connection attempt in progress, wait for it to complete
+        RendezVous rendezVous = oppositeBrokerRdvs.get(port);
+        while (rendezVous != null && rendezVous.isWaitingForConnect()) {
+            // Wait for the current connection attempt to complete
+            try {
+                hostBroker.wait();
+                rendezVous = oppositeBrokerRdvs.get(port);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        oppositeBrokerRdvs.putIfAbsent(port, new RendezVous());
+        rendezVous = oppositeBrokerRdvs.get(port);
+
+        return rendezVous.connect(this);
+    }
+
+    public void delete() {
+        BrokerManager.removeBroker(this);
     }
 }
