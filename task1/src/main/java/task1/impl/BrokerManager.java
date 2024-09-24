d0704af8845f290ed0b82b536d3e695f4793fd54
noahkohrs <noah@kohrs.fr>
2024-09-24 11:40:56 +0200

    fix: BrokerManager now works with an HashMap instead of a Set.

--- task1/src/main/java/task1/impl/BrokerManager.java
+++ task1/src/main/java/task1/impl/BrokerManager.java
@@ -1,25 +1,23 @@
 package task1.impl;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BrokerManager {
 
-    static Set<LocalBroker> brokers = new HashSet<>();
+    static Map<String, LocalBroker> brokers = new HashMap<>();
 
         public static synchronized void addBroker(LocalBroker broker) {
-            brokers.add(broker);
+            brokers.put(broker.name, broker);
         }
 
         public static synchronized void removeBroker(LocalBroker broker) {
-            brokers.remove(broker);
+            brokers.remove(broker.name);
         }
 
         public static synchronized LocalBroker getBroker(String name) {
-            for (LocalBroker broker : brokers) {
-                if (broker.name.equals(name)) {
-                    return broker;
-                }
+            if (brokers.containsKey(name)) {
+                return brokers.get(name);
             }
             throw new IllegalArgumentException("No broker with name " + name);
         }
