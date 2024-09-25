6c4d0291fb4f55d83d667366ce7643fb310cd187
noahkohrs <noah@kohrs.fr>
2024-09-25 12:20:40 +0200

    build: adding few tasks to the build system in advance do avoid doing it again later on.

---
+++ task2/src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void sampleTest() {
+        // This is a sample test
+        // You can write your own tests here
+        Assertions.assertTrue(true);
+    }
+
+}
