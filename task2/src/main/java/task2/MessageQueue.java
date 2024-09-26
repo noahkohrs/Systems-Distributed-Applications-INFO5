0bdf0471315645aaeb2b37527541a2f6ee9a8afb
noahkohrs <noah@kohrs.fr>
2024-09-26 20:34:43 +0200

    docs: updating specs

--- task2/src/main/java/task2/MessageQueue.java
+++ task2/src/main/java/task2/MessageQueue.java
@@ -1,25 +1,39 @@
 package task2;
 
+import task1.exceptions.DisconnectedException;
+
+/**
+ * A {@link Task} that can send and receive bytes.
+ * <br>
+ * It is thread safe and full duplex while respecting a FIFO ordering.
+ * <br>
+ * A message queue is alive until an error occurs, or it is closed via {@link #close()}.
+ */
 public abstract class MessageQueue {
 
     /**
-     * Send the given bytes to the message queue.
+     * Send the given bytes (called message) to the other end of the message queue, which can be received by calling {@link #receive()}.
      * <br>
      * This method is blocking until all the bytes are sent.
      *
-     * @param bytes the bytes to send.
+     * @param bytes  the bytes to send.
      * @param offset the offset in the byte array.
      * @param length the number of bytes to send.
+     * @throws DisconnectedException if the message queue is disconnected.
      */
-    abstract void send(byte[] bytes, int offset, int length);
+    abstract void send(byte[] bytes, int offset, int length) throws DisconnectedException;
 
     /**
      * Receive bytes from the message queue.
      * <br>
-     * This method is blocking until an entire message is received.
+     * This method is blocking until a message is received, which can be sent by calling {@link #send(byte[], int, int)} on the other end.
+     * <br>
+     * The message reading respect a FIFO order.
+     *
      * @return the received bytes.
+     * @throws DisconnectedException if the message queue is disconnected.
      */
-    abstract byte[] receive();
+    abstract byte[] receive() throws DisconnectedException;
 
     /**
      * Close the message queue.
@@ -31,6 +45,7 @@ public abstract class MessageQueue {
 
     /**
      * Check if the message queue is closed.
+     *
      * @return true if the message queue is closed, false otherwise.
      */
     abstract boolean closed();
--- task2/src/main/java/task2/QueueBroker.java
+++ task2/src/main/java/task2/QueueBroker.java
@@ -1,7 +1,13 @@
 package task2;
 
 import task1.Broker;
+import task1.exceptions.ConnectionFailedException;
 
+/**
+ * A {@link QueueBroker} is a network abstraction that can accept incoming connections on a given port and connect to hosts in order.
+ * <br>
+ * The connections resulting for a connection are represented by {@link MessageQueue}.
+ */
 public abstract class QueueBroker {
 
     final Broker broker;
@@ -10,7 +16,27 @@ public abstract class QueueBroker {
         this.broker = broker;
     }
 
+    /**
+     * Get the name of the message queue broker.
+     *
+     * @return the name of the message queue broker.
+     */
     abstract String name();
-    abstract MessageQueue accept(int port);
-    abstract MessageQueue connect(String name, int port);
+
+    /**
+     * Accept a connection on the given port.
+     *
+     * @param port the port to accept the connection on.
+     * @return the message queue associated with the connection.
+     */
+    abstract MessageQueue accept(int port) throws ConnectionFailedException;
+
+    /**
+     * Connect to the message queue broker with the given name.
+     *
+     * @param name the name of the message queue broker.
+     * @param port the port to connect to.
+     * @return the message queue associated with the connection.
+     */
+    abstract MessageQueue connect(String name, int port) throws ConnectionFailedException;
 }
