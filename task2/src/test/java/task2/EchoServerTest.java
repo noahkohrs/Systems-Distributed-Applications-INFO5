83c4697ac7bb0b070cee1bd14887ba1d65c73a52
noahkohrs <noah@kohrs.fr>
2024-10-01 22:03:02 +0200

    fix/refactor: minor changes on the echo server test on the echo server test.

--- task2/src/test/java/task2/EchoServerTest.java
+++ task2/src/test/java/task2/EchoServerTest.java
@@ -20,20 +20,42 @@ public class EchoServerTest extends TitiTotoTesting {
 
     @Test
     void sendingBunchOfDataToTheEchoServer() {
-        var echoServer = new Task(toto, EchoServerTask::new);
+        var echoServer = new Task(toto, this::echoServer);
         echoServer.start();
-        for (var testString : testStrings) {
-            try {
-                var msgQueue = titi.connect("toto", 0);
+        try {
+            var msgQueue = titi.connect("toto", 0);
+            for (var testString : testStrings) {
                 var message = testString.getBytes();
                 msgQueue.send(message, 0, message.length);
                 var received = msgQueue.receive();
                 assertArrayEquals(message, received);
-            } catch (ConnectionFailedException e) {
-                fail("Brokers exists, the connection should be successful", e);
-            } catch (DisconnectedException e) {
-                fail("The echo server should not be disconnected", e);
             }
+        } catch (ConnectionFailedException e) {
+            fail("Brokers exists, the connection should be successful", e);
+        } catch (DisconnectedException e) {
+            fail("The echo server should not be disconnected", e);
         }
+    }
+
+    public void multiEchoServer() {
+        var queueBroker = Task.getQueueBroker();
+        while (true) {
+            new Task(queueBroker, this::echoServer).start();
+        }
+    }
+
+
+    public void echoServer() {
+        var queueBroker = Task.getQueueBroker();
+        try {
+            var messageQueue = queueBroker.accept(0);
+            while (!messageQueue.closed()) {
+                var message = messageQueue.receive();
+                messageQueue.send(message, 0, message.length);
+            }
+        } catch (ConnectionFailedException | DisconnectedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
