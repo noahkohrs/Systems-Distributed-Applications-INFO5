79d38f51023456c50c5f36854a711c3073109196
noahkohrs <noah@kohrs.fr>
2024-10-09 16:56:26 +0200

    refactor: making the message sender and event pump more lightweight while enhancing readability.

--- task3/src/main/java/task3/impl/EventPump.java
+++ task3/src/main/java/task3/impl/EventPump.java
@@ -1,20 +1,31 @@
 package task3.impl;
 
+import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class EventPump {
-    public static Queue<EventTask> queue = new ConcurrentLinkedQueue<>();
+    public static Object locker = new Object();
+    public static Queue<EventTask> queue = new LinkedList<>();
 
     public static void post(EventTask r) {
-        queue.add(r);
+        synchronized (locker) {
+            locker.notify();
+            queue.add(r);
+        }
+    }
+    public static void post(String taskName, Runnable action) {
+        post(new EventTask(taskName, action));
     }
 
     public static void run() throws InterruptedException {
         while (true) {
-            if (!queue.isEmpty()) {
-                EventTask task = queue.poll();
-                task.run();
+            synchronized (locker) {
+                if (!queue.isEmpty()) {
+                    EventTask task = queue.poll();
+                    task.run();
+                } else {
+                    locker.wait();
+                }
             }
         }
     }
--- task3/src/main/java/task3/impl/LocalEventQueueBroker.java
+++ task3/src/main/java/task3/impl/LocalEventQueueBroker.java
@@ -4,6 +4,7 @@ import task1.exceptions.ConnectionFailedException;
 import task1.impl.LocalBroker;
 import task2.MessageQueue;
 import task2.QueueBroker;
+import task2.Task;
 import task2.impl.LocalQueueBroker;
 import task3.EventMessageQueue;
 import task3.EventQueueBroker;
@@ -12,8 +13,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class LocalEventQueueBroker extends EventQueueBroker {
-    final Map<Integer, Thread> binders;
-
+    final Map<Integer, Task> binders;
 
     QueueBroker queueBroker;
 
@@ -35,17 +35,19 @@ public class LocalEventQueueBroker extends EventQueueBroker {
             return false;
         }
 
-        Thread t = new Thread(() -> {
-            while (true) {
-                try {
-                    MessageQueue msgsQueue = queueBroker.accept(port);
-                    EventMessageQueue queue = new LocalEventMessageQueue(msgsQueue);
-                    EventPump.post(new EventTask("Accepting", () -> listener.accepted(queue)));
-                } catch (Exception e) {
-                    break;
-                }
-            }
-        });
+        Task t = new Task(
+                queueBroker,
+                () -> {
+                    while (true) {
+                        try {
+                            MessageQueue msgsQueue = queueBroker.accept(port);
+                            EventMessageQueue queue = new LocalEventMessageQueue(msgsQueue);
+                            EventPump.post(new EventTask("Accepting", () -> listener.accepted(queue)));
+                        } catch (Exception e) {
+                            break;
+                        }
+                    }
+                });
         binders.put(port, t);
         t.start();
         return true;
@@ -63,15 +65,17 @@ public class LocalEventQueueBroker extends EventQueueBroker {
 
     @Override
     public boolean connect(String name, int port, ConnectListener listener) {
-        new Thread(() -> {
-            try {
-                MessageQueue msgsQueue = queueBroker.connect(name, port);
-                EventMessageQueue queue = new LocalEventMessageQueue(msgsQueue);
-                EventPump.post(new EventTask("Connecting", () -> listener.connected(queue)));
-            } catch (ConnectionFailedException e) {
-                EventPump.post(new EventTask("Refusing", listener::refused));
-            }
-        }).start();
+        new Task(
+                queueBroker,
+                () -> {
+                    try {
+                        MessageQueue msgsQueue = queueBroker.connect(name, port);
+                        EventMessageQueue queue = new LocalEventMessageQueue(msgsQueue);
+                        EventPump.post(new EventTask("Connecting", () -> listener.connected(queue)));
+                    } catch (ConnectionFailedException e) {
+                        EventPump.post(new EventTask("Refusing", listener::refused));
+                    }
+                }).start();
         return true;
     }
 }
